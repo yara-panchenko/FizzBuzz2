e73756514234ea58425ba60d037d4e310452e8e4
Opilane TTHK <opilane@tthk.ee>
2021-12-07 10:20:04 +0200

    Add string void validation implementation

--- src/fizzbuzzkata_ipvalidator/ipValidator.java
+++ src/fizzbuzzkata_ipvalidator/ipValidator.java
@@ -2,4 +2,10 @@ package fizzbuzzkata_ipvalidator;
 
 public class ipValidator {
 
+	public Boolean ValidateIpv4Address(String ipString) {
+		if(ipString == "") {
+			return false;
+		}
+		return true;
+	}
 }

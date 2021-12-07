88c1f08b8bda272a97bce60a11fe3df5c79c8c8a
Opilane TTHK <opilane@tthk.ee>
2021-12-07 10:31:17 +0200

    Update ValidateIpv4Address and add hasFourNumbers

--- src/fizzbuzzkata_ipvalidator/ipValidator.java
+++ src/fizzbuzzkata_ipvalidator/ipValidator.java
@@ -4,12 +4,15 @@ import java.util.Arrays;
 public class ipValidator {
 
 	public Boolean ValidateIpv4Address(String ipString) {
-		if(hasThreeDots(ipString)) {
+		if(hasThreeDots(ipString) && 
+				hasFourNumbers(ipString)) {
 			return true;
 		}
 		return false;
 	}
-
+	private boolean hasFourNumbers(String ipString) {
+		return getNumbers(ipString).length == 4;
+	}
 	private boolean hasThreeDots(String ipString) {
 		return ipString.chars().filter(c->c=='.').count()==3;
 	}

84ed4aa236b631f50623b62e0b911f5a9678264a
Opilane TTHK <opilane@tthk.ee>
2021-12-07 10:35:15 +0200

    Update ValidateIpv4Address and add firstNumberInRange1_254

--- src/fizzbuzzkata_ipvalidator/ipValidator.java
+++ src/fizzbuzzkata_ipvalidator/ipValidator.java
@@ -6,10 +6,14 @@ public class ipValidator {
 	public Boolean ValidateIpv4Address(String ipString) {
 		if(hasThreeDots(ipString) && 
 				hasFourNumbers(ipString) &&
-				numbersInRange(ipString)) {
+				numbersInRange(ipString) &&
+				firstNumberInRange1_254(ipString)) {
 			return true;
 		}
 		return false;
+	}
+	private boolean firstNumberInRange1_254(String ipString) {
+		return getNumbers(ipString)[0]>0 && getNumbers(ipString)[0]<255;
 	}
 	private boolean numbersInRange(String ipString) {
 		return Arrays.stream(getNumbers(ipString)).allMatch(nr->nr>=0&&nr<=255);

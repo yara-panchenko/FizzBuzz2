7c7dabaafb473126d9ba80c0ed698a540e4d8b12
Opilane TTHK <opilane@tthk.ee>
2021-12-07 10:25:46 +0200

    Update ValidateIpv4Address and add check for dots in ipString

--- src/fizzbuzzkata_ipvalidator/ipValidator.java
+++ src/fizzbuzzkata_ipvalidator/ipValidator.java
@@ -3,9 +3,13 @@ package fizzbuzzkata_ipvalidator;
 public class ipValidator {
 
 	public Boolean ValidateIpv4Address(String ipString) {
-		if(ipString == "") {
-			return false;
+		if(hasThreeDots(ipString)) {
+			return true;
 		}
-		return true;
+		return false;
+	}
+
+	private boolean hasThreeDots(String ipString) {
+		return ipString.chars().filter(c->c=='.').count()==3;
 	}
 }

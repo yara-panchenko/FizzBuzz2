ae8c3beb0496e16f8f065d94bcb5ab71484eb5fe
yara-panchenko <62282226+yara-panchenko@users.noreply.github.com>
2021-12-08 09:59:58 +0200

    Update names of classes

--- src/fizzbuzzkata_ipvalidator/ipValidatorTests.java
+++ src/fizzbuzzkata_ipvalidator/ipValidatorTests.java
@@ -8,29 +8,29 @@ class ipValidatorTests {
 
 	@Test
 	void ShouldReturnFalse_GivenEmptyStringParameter() {
-		ipValidator validator = new ipValidator();
+		IpValidator validator = new IpValidator();
 		assertFalse(validator.ValidateIpv4Address(""));
 	}
 	@Test
 	void ShouldBe_False_GivenStringWithOutThreeDots() {
-		ipValidator validator = new ipValidator();
+		IpValidator validator = new IpValidator();
 		assertFalse(validator.ValidateIpv4Address(".."));
 		assertFalse(validator.ValidateIpv4Address("...."));
 	}
 	@Test
 	void ShouldReturnFourNumbersGivenStringWithThreeDots() {
-		ipValidator validator = new ipValidator();
+		IpValidator validator = new IpValidator();
 		int[] expected = {1,2,3,4};
 		assertArrayEquals(expected,validator.getNumbers("1.2.3.4"));
 	}
 	@Test
 	void ShouldBe_False_GivenStringWithThreeNumbersAndDots() {
-		ipValidator validator = new ipValidator();
+		IpValidator validator = new IpValidator();
 		assertFalse(validator.ValidateIpv4Address("1.2.3."));
 	}
 	@Test
 	void ShouldBe_False_GivenStringWithFourNumbersNotInRange0_255(){
-		ipValidator validator = new ipValidator();
+		IpValidator validator = new IpValidator();
 		assertFalse(validator.ValidateIpv4Address("1.2.3.257"));
 		assertFalse(validator.ValidateIpv4Address("1.2.300.4"));
 		assertFalse(validator.ValidateIpv4Address("1.256.3.4"));
@@ -38,23 +38,23 @@ class ipValidatorTests {
 	}
 	@Test
 	void ShouldBe_False_GivenIPstartingWithZero() {
-		ipValidator validator= new ipValidator();
+		IpValidator validator= new IpValidator();
 		assertFalse(validator.ValidateIpv4Address("0.2.3.4"));
 	}
 	@Test
 	void ShouldBe_False_GivenIPendingWithZero() {
-		ipValidator validator = new ipValidator();
+		IpValidator validator = new IpValidator();
 		assertFalse(validator.ValidateIpv4Address("1.2.3.0"));
 	}
 	@Test
 	void ShouldBe_False_GivenIPstartingWith255() {
-		ipValidator validator= new ipValidator();
+		IpValidator validator= new IpValidator();
 		assertFalse(validator.ValidateIpv4Address("255.2.3.4"));
 	}
 
 	@Test
 	void ShouldBe_False_GivenIPendingWith255() {
-		ipValidator validator = new ipValidator();
+		IpValidator validator = new IpValidator();
 		assertFalse(validator.ValidateIpv4Address("1.2.3.255"));
 	}
 
